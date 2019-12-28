9d2876005591fde38f02d151221a4695dc6843eb
권혜주 <s2dlgosls2@naver.com>
2019-12-28 14:41:27 +0900

    콜백테스트

--- src/componants/Header.js
+++ src/componants/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Stats} from "./Stats";
-import {Stopmatch} from "./Stopmatch";
+import {Stopwatch} from "./Stopwatch";
 
 export const Header = (props) => {
   console.log(props);
@@ -8,7 +8,7 @@ export const Header = (props) => {
     <header className="header">
       <Stats players={props.players} />
       <h1 className="h1">{props.title}</h1>
-      <Stopmatch></Stopmatch>
+      <Stopwatch></Stopwatch>
     </header>
   );
 }
