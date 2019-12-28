743bf6373d428df46f51106ef868173d45256c78
권혜주 <s2dlgosls2@naver.com>
2019-12-28 12:33:11 +0900

    3주차

--- src/componants/AddPlayerForm.js
+++ src/componants/AddPlayerForm.js
@@ -12,6 +12,7 @@ export class AddPlayerForm extends React.Component {
     this.setState({value:''});
   }
 
+
   handleValueChange =(e)=>{
     console.log(e);
     this.setState({value: e.target.value});
